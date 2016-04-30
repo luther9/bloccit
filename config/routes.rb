6c22cd5999c69b572757a6e1929158c84fa27e9d
Luther <lutheroto@gmail.com>
2016-04-30 11:58:42 -0400

    Generate Questions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'questions/edit'
+
+  get 'questions/index'
+
+  get 'questions/new'
+
+  get 'questions/show'
+
   resources :posts
 
   get 'about' => 'welcome#about'

1bf1d5d8e5446f372704b73f64adb7ee0765ed9d
Luther <lutheroto@gmail.com>
2016-05-04 09:51:55 -0400

    Write routes for SponsoredPost.

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -1,24 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
+  let :my_topic do
+    Topic.create! name: RandomData.random_sentence, description: RandomData.random_paragraph
+  end
+  let :my_sponsored_post do
+    my_topic.sponsored_posts.create! title: RandomData.random_sentence, body: RandomData.random_paragraph
+  end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, topic_id: my_topic.id, id: my_sponsored_post.id
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, topic_id: my_topic.id
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get :edit, topic_id: my_topic.id, id: my_sponsored_post.id
       expect(response).to have_http_status(:success)
     end
   end

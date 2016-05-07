611d4e4f4444ac860768b00b50eb2684fdd8f581
Luther <lutheroto@gmail.com>
2016-05-07 03:02:26 -0400

    Add validations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_group_tag errors, &block
+    css_class = 'form-group'
+    if errors.any?
+      css_class << ' has-error'
+    end
+    content_tag :div, capture(&block), class: css_class
+  end
 end

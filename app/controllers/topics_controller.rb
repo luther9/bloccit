ec75a950adf25904f3e9ea9e751fbd2a9f1d463b
Luther <lutheroto@gmail.com>
2016-05-20 23:55:20 -0400

    Pass TopicsController tests.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -18,7 +18,7 @@ class TopicsController < ApplicationController
     @topic = Topic.new topic_params
 
     if @topic.save
-      @topics.labels = Label.update_labels params[:topic][:labels]
+      @topic.labels = Label.update_labels params[:topic][:labels]
       flash[:notice] = 'Topic was saved successfully.'
       redirect_to @topic
     else
@@ -37,7 +37,7 @@ class TopicsController < ApplicationController
     @topic.assign_attributes topic_params
 
     if @topic.save
-      @topics.labels = Label.update_labels params[:topic][:labels]
+      @topic.labels = Label.update_labels params[:topic][:labels]
       flash[:notice] = 'Topic was updated successfully.'
       redirect_to @topic
     else

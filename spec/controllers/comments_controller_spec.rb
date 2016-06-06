062ea86ea6fbe15915a58685b37cb713615acbf3
Luther <lutheroto@gmail.com>
2016-06-05 23:01:48 -0400

    Use Ajax.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -21,14 +21,14 @@ RSpec.describe CommentsController, type: :controller do
   context 'guest' do
     describe 'POST create' do
       it 'redirects the user to the sign in view' do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
         expect(response).to redirect_to new_session_path
       end
     end
 
     describe 'DELETE destroy' do
       it 'redirects the user to the sign in view' do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
         expect(response).to redirect_to new_session_path
       end
     end
@@ -42,19 +42,19 @@ RSpec.describe CommentsController, type: :controller do
     describe 'POST create' do
       it 'increases the number of comments by 1' do
         expect do
-          post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+          post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
         end.to change(Comment, :count).by 1
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+        expect(response).to have_http_status :success
       end
     end
 
     describe 'DELETE destroy' do
       it 'redirects the user to the posts show view' do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
         expect(response).to redirect_to [my_topic, my_post]
       end
     end
@@ -68,19 +68,19 @@ RSpec.describe CommentsController, type: :controller do
     describe 'POST create' do
       it 'increases the number of comments by 1' do
         expect do
-          post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+          post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
         end.to change(Comment, :count).by 1
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+        expect(response).to have_http_status :success
       end
     end
 
     describe 'DELETE destroy' do
       before :each do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
       end
 
       it 'deletes the comment' do
@@ -88,8 +88,8 @@ RSpec.describe CommentsController, type: :controller do
         expect(count).to eq 0
       end
 
-      it 'redirects to the post show view' do
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        expect(response).to have_http_status :success
       end
     end
   end
@@ -103,19 +103,19 @@ RSpec.describe CommentsController, type: :controller do
     describe 'POST create' do
       it 'increases the number of comments by 1' do
         expect do
-          post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+          post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
         end.to change(Comment, :count).by 1
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+        expect(response).to have_http_status :success
       end
     end
 
     describe 'DELETE destroy' do
       before :each do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
       end
 
       it 'deletes the comment' do
@@ -123,8 +123,8 @@ RSpec.describe CommentsController, type: :controller do
         expect(count).to eq 0
       end
 
-      it 'redirects to the post show view' do
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        expect(response).to have_http_status :success
       end
     end
   end

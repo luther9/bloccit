3aa9669f0fe589bfceef3f83fb7cd31683e3d075
Luther <lutheroto@gmail.com>
2016-06-08 13:37:16 -0400

    Add a test.

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe(Api::V1::TopicsController, type: :controller) {
   let(:my_user) { create :user }
-  let(:my_topic) { create :topic }
+  let!(:my_topic) { create :topic }
+  let!(:my_post) { create :post, topic: my_topic}
 
   context("unauthenticated user") {
     it("GET index returns http success") {
       get :index
       expect(response).to have_http_status :success
+    }
+
+    it('GET show returns topic and posts') {
+      get :show, id: my_topic.id
+      expect(response.body).to eq [my_topic, my_topic.posts].to_json
     }
 
     it("GET show returns http success") {
